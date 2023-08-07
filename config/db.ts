0c3677009baf547e292a4d5383318a643f54f69b
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2023-08-07 22:41:29 +0100

    pushed by francis

--- config/db.ts
+++ config/db.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const URL: string = "mongodb://127.0.0.1:27017/Group";
 
- const db = () => {
+const db = () => {
   mongoose.connect(URL).then(() => {
     console.log("connected");
   });
 };
 
-export default db
+export default db;
