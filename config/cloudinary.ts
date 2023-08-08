907f01f82fc58f114431027001fec775e65657a9
Elizabeth Okorie <elizabethokorie407@gmail.com>
2023-08-08 02:15:35 +0100

    solved .env

--- config/cloudinary.ts
+++ config/cloudinary.ts
@@ -1,9 +1,11 @@
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv"
+dotenv.config()
 
 cloudinary.config({
-  cloud_name: "dhu54k4vh",
-  api_key: "592862638221346",
-  api_secret: "se_JzaK_qd4vPqCnnMVxXppdHmA",
+  cloud_name: process.env.CLOUD_NAME!,
+  api_key: process.env.API_KEY!,
+  api_secret: process.env.API_SECRET!,
 });
 
 export default cloudinary;
